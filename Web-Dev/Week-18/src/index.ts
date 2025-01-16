92bf0fa20e8e24998fd55ca4d6b3cf09bc78dede
Laxman Singh Koranga <korangalaxman03@gmail.com>
2025-01-16 15:00:34 +0530

    [Web-Dev:Week-18]: Added migration.

--- Web-Dev/Week-18/src/index.ts
+++ Web-Dev/Week-18/src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello, Prisma!");
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+// creating user.
+async function getUser() {
+    const user = await client.user.findFirst({
+        where: {
+            id: 1
+        },
+        include: {
+            todos: true
+        }
+    })
+    console.log(user);
+    
+}
+
+getUser();
